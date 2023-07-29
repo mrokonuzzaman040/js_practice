3eb9e2735ef62d89e19dface864c878e8e36ff6a
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-29 23:57:57 +0600

    conditional decision

--- Day-2/condition.js
+++ Day-2/condition.js
@@ -4,6 +4,9 @@ var myBudget = 800;
 if (myBudget >= iphoneBudget) {
     console.log("I can buy the phone");
 }
+else if (myBudget == iphoneBudget) {
+    console.log("I can buy the phone but I need to think");
+}
 else {
     console.log("I can't buy the phone");
 }
