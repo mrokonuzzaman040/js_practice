86a2ab5c163277a25a3964650284519426ba1f72
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-30 00:15:09 +0600

    2 condition system

---
+++ Day-2/multiple-conditions2.js
@@ -0,0 +1,10 @@
+var isGraduate = true;
+var salary = 75000;
+var car = 1;
+
+if (isGraduate === true || salary > 50000 && car === 1) {
+    console.log("You are hired!");
+}
+else {
+    console.log("Sorry, you are not hired.");
+}
