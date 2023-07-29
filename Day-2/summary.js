c30a1639a366f441053ab6006248bc751b8e52dd
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-30 00:33:01 +0600

    summary

---
+++ Day-2/summary.js
@@ -0,0 +1,6 @@
+var destination = ['sajeck', 'coxbazar', 'sundarban', 'saintmartin', 'kuakata', 'bandarban', 'rangamati', 'khagrachori', 'sylhet', 'rajshahi', 'rangpur', 'dinajpur', 'bogura', 'khulna', 'barishal', 'mymensingh', 'chittagong', 'dhaka'];
+
+console.log(destination.length);
+destination[1] = 'srilanka';
+var index = destination.indexOf('dhaka');
+console.log(index);
