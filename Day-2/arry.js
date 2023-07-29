b4ae510946986fa86bb8be044673ea9ac76e2331
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-29 22:26:38 +0600

    Arry Update

--- Day-2/arry.js
+++ Day-2/arry.js
@@ -1,1 +1,7 @@
-var friendsAge = [15, 17, 14, 16];
+var friendsName = ['Rahim', 'Karim', 'Jabbar', 'Salam', 'Borkot'];
+var friendsAge = [11, 12, 13, 14, 15];
+var friendsRoll = [21, 22, 23, 24, 25];
+var friendsClass = [6, 7, 8, 9, 10];
+
+var friends = ['Rahim', 11, 21, 6, 'Karim', 12, 22, 7, 'Jabbar', 13, 23, 8, 'Salam', 14, 24, 9, 'Borkot', 15, 25, 10];
+console.log(friends);
