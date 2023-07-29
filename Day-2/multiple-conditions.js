6d9ec96280aa171ba3883c4790eb9ac3c324b9f6
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-30 00:09:05 +0600

    multiple conditions update

---
+++ Day-2/multiple-conditions.js
@@ -0,0 +1,10 @@
+var isGraduate = true;
+var salary = 25000;
+var cars = 1;
+
+if (isGraduate == true && salary > 20000 && cars >= 1) {
+    console.log('You are a graduate');
+}
+else {
+    console.log('You are not a graduate');
+}
