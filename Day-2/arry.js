1209c0f559bd8980872d275d9ca31574bd1294b8
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-29 22:35:12 +0600

    Update This File

--- Day-2/arry.js
+++ Day-2/arry.js
@@ -2,3 +2,7 @@ var friendsName = ['Rahim', 'Karim', 'Jabbar', 'Salam', 'Borkot'];
 var friendsAge = [11, 12, 13, 14, 15];
 var friendsRoll = [21, 22, 23, 24, 25];
 var friendsClass = [6, 7, 8, 9, 10];
+
+console.log(friendsName[0]);
+
+//all arry element start 0 index number and last index number is arry length -1 
