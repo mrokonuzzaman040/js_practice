0287f3118100d9d9423078c99c2a58fe7d6d36f7
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-29 23:28:47 +0600

    Push and Pop update

---
+++ Day-2/push_pop.js
@@ -0,0 +1,28 @@
+var numbers = [78, 5, 20, 10, 8, 9, 45, 100];
+console.log(numbers);
+
+// use push method to add element in arry
+// push element in arry
+numbers.push(200);
+console.log(numbers);
+
+// use pop method to remove element from arry
+// pop element from arry
+numbers.pop();
+console.log(numbers);
+
+// use shift method to remove element from arry
+// shift element from arry
+numbers.shift();
+console.log(numbers);
+
+// use unshift method to add element in arry
+// unshift element in arry
+numbers.unshift(1);
+console.log(numbers);
+
+// use splice method to add element in arry
+// slice element from arry
+var sliceElement = numbers.slice(2, 5);
+console.log(sliceElement);
+console.log(numbers);
