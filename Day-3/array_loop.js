625763be436cc7737eb3db98e44ec0f92a7a3c72
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-30 23:47:25 +0600

    Arry Loop update

---
+++ Day-3/array_loop.js
@@ -0,0 +1,20 @@
+// for (var i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+// target: Display every element of an array
+var numbers = [55, 66, 77, 88, 99, 11, 44];
+
+
+// for( var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var items = ['Bottle', 'Mouse', 'Sunglass', 'Pen']
+
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
