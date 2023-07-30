635127ec52f42849ffb3fa00c180831e118f368a
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-31 00:00:56 +0600

    break and continue added

---
+++ Day-3/contnue.js
@@ -0,0 +1,12 @@
+var numbers = [55, 66, 77, 88, 99, 11, 44];
+
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    if(number > 70){
+        continue;
+    }
+    console.log(number)
+}
+
+
+var items = ['Bottle', 'Mouse', 'Sunglass', 'Pen']
