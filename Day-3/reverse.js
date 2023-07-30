6c24c91fcd482700273714deada2fcf0f0b36bd1
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-31 00:09:14 +0600

    Reverse loop done

---
+++ Day-3/reverse.js
@@ -0,0 +1,25 @@
+//Forword for Loop
+var number = 1;
+
+while(number <= 10){
+    console.log(number);
+    number++;
+}
+
+//reverse for loop
+var num = 10;
+
+while( num >= 1){
+    console.log(num);
+    num--;
+}
+
+// foreword for loop
+for (var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// reverse for loop
+for (var i = 10; i >= 1; i--){
+    console.log(i);
+}
