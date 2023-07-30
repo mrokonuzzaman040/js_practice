34ac97df1bedf65b5a182787c2da9533997c18ad
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-30 23:32:14 +0600

    for loop done

--- Day-3/for-loop.js
+++ Day-3/for-loop.js
@@ -1,5 +1,14 @@
+// as we know for loop is a loop that runs for a limited time
 var rostGiven = 0;
 
 for (var rost = 1; rost <= 5; rost++) {
     console.log('Road Den Please');
+}
+
+// simple version of for loop
+var rostGiven = 0;
+
+for (var rost = 1; rost <= 5; rost++) {
+    console.log('Road Den Please');
+    console.log(rost);
 }
