c94c7a2dd5ea02d48658d4e5d1d04091c20d31b3
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-08-02 21:46:28 +0600

    object added

---
+++ Day-4/object.js
@@ -0,0 +1,14 @@
+var student = {
+    id: 115, 
+    name: "Rokon", 
+    Class: "BSc in CSE", 
+    Markes: 98
+}
+
+
+var mobile = {
+    brand : "Samsung",
+    price : 25000,
+    storage : "16GB",
+    camera : "13MP"
+}
