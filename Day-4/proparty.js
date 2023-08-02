b445f9853fac318bb395cce034cade492a9da3cf
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-08-02 23:04:34 +0600

    proparty added

---
+++ Day-4/proparty.js
@@ -0,0 +1,18 @@
+var shopingCart = {
+    books: 3,
+    sunglasses: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25,
+}
+
+var prnCount = shopingCart.pen;
+
+var penCount2 = shopingCart['pen'];
+
+var propartyName = Object.keys(shopingCart);
+var propartyValue = Object.values(shopingCart);
+
+console.log(propartyName);
+console.log(propartyValue);
+// console.log(penCount2);
